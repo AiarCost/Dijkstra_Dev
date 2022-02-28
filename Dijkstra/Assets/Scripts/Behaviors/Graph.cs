c2b6a88f4bc1c688602915c7a2e2f498a67f590c
AiarCost <65986842+AiarCost@users.noreply.github.com>
2022-02-28 11:01:28 -0700

    initial project

---
+++ Dijkstra/Assets/Scripts/Behaviors/Graph.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph 
+{
+    List<Connection> mConnections;
+
+    public List<Connection> GetConnections(Node fromNode)
+    {
+        List<Connection> connections = new List<Connection>();
+        foreach(Connection c in mConnections)
+        {
+            if(c.GetFromNode() == fromNode)
+            {
+                connections.Add(c);
+            }
+        }
+
+        return connections;
+    }
+
+    public void Build()
+    {
+        mConnections = new List<Connection>();
+
+        Node[] nodes = GameObject.FindObjectsOfType<Node>();
+
+        foreach (Node fromNode in nodes)
+        {
+            foreach (Node toNode in fromNode.ConnectsTo)
+            {
+                float cost = 1;
+                Connection c = new Connection(cost, fromNode, toNode);
+                mConnections.Add(c);
+            }
+        }
+    }
+}
+
+
+public class Connection
+{
+    float cost;
+    Node fromNode;
+    Node toNode;
+
+    public Connection(float cost, Node fromNode, Node toNode)
+    {
+        this.cost = cost;
+        this.fromNode = fromNode;
+        this.toNode = toNode;
+    }
+    public float GetCost() { return cost; }
+
+   public Node GetFromNode() { return fromNode; }
+
+    public Node GetToNode() { return toNode; }
+}
